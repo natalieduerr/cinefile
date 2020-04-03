c57f35b3a67d8a91390e72d00a26b6e798a0d133
natalieduerr <natalieduerr98@gmail.com>
2020-04-03 18:03:43 -0400

    started front-end work

---
+++ src/components/film/FilmCard.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+// import './FilmCard.scss';
+import Card from "@material-ui/core/Card/Card";
+import CardActionArea from "@material-ui/core/CardActionArea/CardActionArea";
+import CardMedia from "@material-ui/core/CardMedia/CardMedia";
+import CardContent from "@material-ui/core/CardContent/CardContent";
+import Grid from '@material-ui/core/Grid';
+
+import CardActions from '@material-ui/core/CardActions';
+import IconButton from '@material-ui/core/IconButton';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+
+// import {filmPoster} from "../../constants";
+import Poster from "./little-women.jpg";
+import './filmcard.scss'
+
+export default class FilmCard extends React.Component {
+  // constructor(props) {
+  //     super(props);
+  // }
+
+  render() {
+    return (
+      <Grid item>
+        <Card className={'film-card'}>
+          <CardActionArea className={'film-card--action'}>
+            <CardMedia className={'film-card--poster'}>
+              <img src={Poster} alt="Little Women"></img>
+              {/* {this.props.film.title}></img> */}
+            </CardMedia>
+            <CardContent className={'film-card--info'}>
+              {/* <h2>{this.props.film.title}</h2> */}
+              <h2>Little Women</h2>
+              {/* do if statement in case no year */}
+              {/* <h3>{this.props.film.year}</h3> */}
+              <h3>2019</h3>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+    );
+  }
+}
---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import './navigation.scss';
+import { Link } from "react-router-dom";
+
+export default class Navigation extends React.Component {
+
+    render() {
+        return (
+            <BrowserRouter>
+                    <Grid className={'nav'} 
+                        container
+                        item
+                        justify={'center'}
+                        alignItems={'flex-start'}
+                        direction={'column'}
+                        xs={3}
+                        spacing={3} >
+                        <Grid item>
+                            <Link to='/explore'>
+                                <img src={'/imgs/logo.png'} alt={'logo'} width={200} />
+                            </Link>
+                        </Grid>
+                        <Grid item className={this.props.activeTab === 'explore' ? 'active' : null}>
+                            <Link to='/explore'>Explore</Link>
+                        </Grid>
+                        <Grid item className={this.props.activeTab === 'add' ? 'active' : null}>
+                            <Link to='/film/create'>Add Film</Link>
+                        </Grid>
+                        <Grid item className={this.props.activeTab === 'account' ? 'active' : null}>
+                            <Link to='/account'>Account</Link>
+                        </Grid>
+                    </Grid>
+            </BrowserRouter>
+        );
+    }
+}
