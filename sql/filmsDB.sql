a2ca004093438a35dae1e9596f3c370c2d953033
abbycarr <carr.ab@husky.neu.edu>
2020-04-11 00:58:53 -0500

    initial complete set of films db

--- sql/filmsDB.sql
+++ sql/filmsDB.sql
@@ -2,33 +2,34 @@ CREATE DATABASE films;
 USE films;
 
 CREATE TABLE award (
-	id INT PRIMARY KEY,
+	id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(45) NOT NULL);
     
 CREATE TABLE film_festival (
-	id INT PRIMARY KEY,
+	id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(45) NOT NULL,
     location VARCHAR(45) NOT NULL,
     year_started INT NOT NULL);
     
 CREATE TABLE genre (
-	id INT PRIMARY KEY,
+	id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(45) NOT NULL);
     
 CREATE TABLE director (
-	id INT PRIMARY KEY,
+	id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(45) NOT NULL,
     birthdate DATE NOT NULL,
     gender VARCHAR(45) NOT NULL,
     race VARCHAR(45) NOT NULL);
     
 CREATE TABLE film (
-	id INT PRIMARY KEY,
+	id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(200) NOT NULL,
     date_released DATE NOT NULL,
     passes_bechdol BOOLEAN NOT NULL,
     runtime INT NOT NULL,
-    rating INT NOT NULL,
+    rating VARCHAR(45),
+    photo VARCHAR(45) DEFAULT 'noPic.jpg',
     director INT REFERENCES director(id)
 		ON UPDATE CASCADE
 		ON DELETE CASCADE,
@@ -37,7 +38,7 @@ CREATE TABLE film (
 		ON DELETE CASCADE);
 
 CREATE TABLE winner (
-	id INT PRIMARY KEY,
+	id INT PRIMARY KEY AUTO_INCREMENT,
     year INT NOT NULL,
     film INT REFERENCES film(id)
 		ON UPDATE CASCADE
@@ -47,7 +48,7 @@ CREATE TABLE winner (
 		ON DELETE CASCADE);
     
 CREATE TABLE debuted_at_festival (
-	id INT PRIMARY KEY,
+	id INT PRIMARY KEY AUTO_INCREMENT,
     film INT REFERENCES film(id)
     	ON UPDATE CASCADE
 		ON DELETE CASCADE,
@@ -56,10 +57,8 @@ CREATE TABLE debuted_at_festival (
 		ON DELETE CASCADE);
     
 CREATE TABLE watched (
-	id INT PRIMARY KEY,
-    user VARCHAR(45) REFERENCES user(username)
-    	ON UPDATE CASCADE
-		ON DELETE CASCADE,
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    user VARCHAR(45) NOT NULL,
     date DATE NOT NULL,
     rating INT,
     film INT REFERENCES film(id)
@@ -68,6 +67,3 @@ CREATE TABLE watched (
 	CHECK (rating = NULL or rating = 1 OR rating = 2 OR rating = 3 
 			OR rating = 4 OR rating = 5));
             
-CREATE TABLE user (
-	username VARCHAR(45) NOT NULL,
-    password VARCHAR(45) NOT NULL);
